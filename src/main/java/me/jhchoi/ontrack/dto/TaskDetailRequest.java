89446f8127984743fa7b9f3bcee4e3dc32cc0baa
leaven153 <busmoja@naver.com>
2024-07-18 01:57:10 +0900

    240717 25:55 - 인텔리제이의 경고에 따라 EqualsAndHashCode어노테이션 추가

--- src/main/java/me/jhchoi/ontrack/dto/TaskDetailRequest.java
+++ src/main/java/me/jhchoi/ontrack/dto/TaskDetailRequest.java
@@ -1,15 +1,13 @@
 package me.jhchoi.ontrack.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import me.jhchoi.ontrack.domain.TaskComment;
 import me.jhchoi.ontrack.domain.TaskFile;
 import me.jhchoi.ontrack.domain.TaskHistory;
 
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true) // 경고:(13, 1) 이 클래스가 java.lang.Object를 확장하지 않은 경우에도 상위 클래스를 호출하지 않고 equals/hashCode 구현을 생성합니다. 의도적인 경우에는 타입에 '(call Super=false)'를 추가하세요.
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
