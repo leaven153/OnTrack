09d50f2b7f2e105e5c6683c8c86a9eb02a98407f
leaven153 <busmoja@naver.com>
2024-08-21 01:59:41 +0900

    240820 25:57 - 사용하지 않는 import문 정리

--- src/main/java/me/jhchoi/ontrack/domain/ProjectMember.java
+++ src/main/java/me/jhchoi/ontrack/domain/ProjectMember.java
@@ -1,13 +1,12 @@
 package me.jhchoi.ontrack.domain;
 
-import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 
 @Data @NoArgsConstructor
