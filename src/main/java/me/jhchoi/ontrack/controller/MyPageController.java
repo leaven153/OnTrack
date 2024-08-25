c02cb2044ed9eb3133e8f079eebd19974f258336
leaven153 <busmoja@naver.com>
2024-08-26 01:21:07 +0900

    240826 01:20 - ErrorResponse를 builder패턴으로 변경(@MyPageController)

--- src/main/java/me/jhchoi/ontrack/controller/MyPageController.java
+++ src/main/java/me/jhchoi/ontrack/controller/MyPageController.java
@@ -102,7 +102,7 @@ public class MyPageController {
         log.info("휴지통의 할 일 row 동적으로 생성하기 위해 컨트롤러 접근 -task id: {}", taskId);
         BinResponse binTaskRow = taskService.binTaskRow(taskId);
         if(binTaskRow == null) {
-            return ResponseEntity.badRequest().body(new ErrorResponse("해당 할 일이 존재하지 않습니다."));
+            return ResponseEntity.badRequest().body(ErrorResponse.builder().message("해당 할 일이 존재하지 않습니다.").removed(true).build());
         }
         return ResponseEntity.ok().body(binTaskRow);
     }
